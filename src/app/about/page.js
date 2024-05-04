c5a8f1a9957252b6ca1612051c54ad4f371469a9
Ali Aalam <aliaalam.me@gmail.com>
2024-05-04 08:49:44 +0500

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function page() {
+    return (
+        <div>
+            <div className=''>
+                <Navbar />
+
+                <h1>This is About page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default page
